0f13cd2a90de0624433a620252416cb94ec0c413
SinCera_w <iiajia4ex@gmail.com>
2021-09-24 14:27:18 +0300

    first commit and a new project were created

---
+++ Exercises/Exercise1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        //С заданием справился без каких-либо трудностей.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Пожалуйста, укажите своё имя");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Привет, {userName}, сегодня {DateTime.Today}");
+
+            Console.ReadLine();
+        }
+    }
+}
