6b639b092b83bc1d0b654678379c35cf4ea86e93
SinCera_w <iiajia4ex@gmail.com>
2021-09-24 14:53:17 +0300

    added info

--- Exercises/Exercise1/Program.cs
+++ Exercises/Exercise1/Program.cs
@@ -5,7 +5,7 @@ namespace Exercise1
     class Program
     {
         /*С заданием справился без каких-либо трудностей.
-         *Трудности возникли с GIT, Долго не могк понять как сделать pull-request
+         *Трудности возникли с GIT, Долго не мог понять как сделать pull-request
         */
         static void Main(string[] args)
         {
