3d941f331fac60635d7aaab7c725530173de1ea5
SinCera_w <iiajia4ex@gmail.com>
2021-10-04 15:39:57 +0300

    Added Lesson 4 exercises

---
+++ Exercises/Lesson3_Exercise1/DiagonalOut.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DiagonalArrayItemsOut
+{
+    class DiagonalOut
+    {
+        static void Main(string[] args)
+        {
+            var dd = new int [5,5];
+
+            dd[0, 0] = 1;
+            dd[1, 1] = 2;
+            dd[2, 2] = 3;
+            dd[3, 3] = 4;
+            dd[4, 4] = 5;
+            
+
+            var counter = 0;
+
+            for (int i = 0;  i < dd.GetLength(0); i++)
+            {
+                
+                for (int j = 0; j < 1 && counter < dd.GetLength(1); j ++)
+                {
+
+                    Console.WriteLine(dd[i, counter++]);
+
+                }
+
+                
+            }
+           
+        }
+    }
+}
---
+++ Exercises/Lesson3_Exercise3/ArrReverse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReverseArrayInput
+{
+    class ArrReverse
+    {
+        static void Main(string[] args)
+        {
+            var userString = Console.ReadLine();
+
+            var tmpArr = userString.ToCharArray();
+
+            for (int i = tmpArr.GetUpperBound(0); i >= tmpArr.GetLowerBound(0); i--)
+            {
+                Console.Write(tmpArr[i]);
+            }
+        }
+    }
+}
