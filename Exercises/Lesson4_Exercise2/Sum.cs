274abf7962b4941e3e6f572c5f9d23735e344a91
SinCera_w <iiajia4ex@gmail.com>
2021-10-06 17:07:23 +0300

    Added exercises for lesson 4

---
+++ Exercises/Lesson4_Exercise2/Sum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NumbersSum
+{
+    class Sum
+    {
+
+
+        static void GetSum(string numbers)
+        {
+            var tmp = numbers.Split(" ");
+            var sum = 0;
+
+            for(var i = 0; i <= tmp.GetUpperBound(0); i++)
+            {
+                sum += Convert.ToInt32(tmp[i]);
+            }
+
+            Console.WriteLine($"Сумма числел равна: {sum}");
+        }
+
+
+        static void Main(string[] args)
+        {
+            GetSum(Console.ReadLine());
+        }
+
+
+    }
+}
---
+++ Exercises/Lesson4_Exercise4/GetFibonacci .cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+
+        public static long Fibo(long number)
+        {
+          
+
+            if(number == 0)
+            {
+                return 0;
+            }
+
+            if(number == 1)
+            {
+                return 1;
+            }
+
+            return Fibo(number - 1) + Fibo(number-2);
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(Fibo(19));
+
+
+        }
+    }
+}
+
