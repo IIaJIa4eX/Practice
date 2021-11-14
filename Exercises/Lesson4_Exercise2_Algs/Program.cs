61fd57daab8e7ab082602eddaa09d095b967fe52
IIaJIa4eX <iiajia4ex@gmail.com>
2021-11-14 21:54:22 +0300

    Added partial implementation for exercise 2(Lesson4)

---
+++ Exercises/Lesson4_Exercise2_Algs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson4_Exercise2_Algs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var tree = new TreeLogic();
+            tree.AddItem(10);
+            tree.AddItem(11);
+            tree.AddItem(9);
+            tree.AddItem(6);
+            tree.AddItem(5);
+            tree.AddItem(30);
+            tree.AddItem(12);
+            tree.AddItem(15);
+
+            var node = tree.GetNodeByValue(5);
+            bool ss = true;
+            
+        }
+    }
+}
