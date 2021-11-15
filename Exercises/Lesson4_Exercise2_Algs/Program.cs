f3dafeee4992337d13cea829a193b55ee9036fcd
SinCera_w <iiajia4ex@gmail.com>
2021-11-15 10:02:58 +0300

    files removed

---
+++ Exercises/Lesson4_Exercise2_Algs/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Lesson4_Exercise2_Algs
+{
+
+
+
+    public interface ITree
+    {
+        TreeNode GetRoot();
+        void AddItem(int value); // добавить узел
+        void RemoveItem(int value); // удалить узел по значению
+        TreeNode GetNodeByValue(int value); //получить узел дерева по значению
+        void PrintTree(); //вывести дерево в консоль
+    }
+
+    public class TreeNode
+    {
+        public int Value { get; set; }
+        public TreeNode LeftChild { get; set; }
+        public TreeNode RightChild { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var node = obj as TreeNode;
+
+            if (node == null)
+                return false;
+
+            return node.Value == Value;
+        }
+    }
+
+
+    public class NodeInfo
+    {
+        public int Depth { get; set; }
+        public TreeNode Node { get; set; }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
