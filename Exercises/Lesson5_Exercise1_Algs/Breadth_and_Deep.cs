81ce427b91639eb710d970a344c95523541509f6
SinCera_w <iiajia4ex@gmail.com>
2021-11-16 17:06:59 +0300

    Added lesson 5

---
+++ Exercises/Lesson5_Exercise1_Algs/Breadth_and_Deep.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+
+namespace Lesson5_Exercise1_Algs
+{
+    class Breadth_and_Deep
+    {
+        static void Main(string[] args)
+        {
+            var tree = new TreeLogic_Coll();
+            tree.AddItem(13);
+            tree.AddItem(11);
+            tree.AddItem(15);
+            tree.AddItem(10);
+            tree.AddItem(12);
+            tree.AddItem(14);
+            tree.AddItem(16);
+
+
+            Console.WriteLine("Ширина");
+            var queTree = new Queue();
+            //Обход в ширину
+            var current = tree.GetRoot();
+            queTree.Enqueue(current);
+
+            while(queTree.Count != 0)
+            {
+                TreeNode_Coll tmp = (TreeNode_Coll)queTree.Dequeue();
+                Console.WriteLine(tmp.Value);
+
+                if(tmp.LeftChild != null)
+                {
+                    queTree.Enqueue(tmp.LeftChild);
+                }
+                if (tmp.RightChild != null)
+                {
+                    queTree.Enqueue(tmp.RightChild);
+                }
+
+            }
+
+            Console.WriteLine("_____________________________");
+            Console.WriteLine("Глубина");
+            //Обход в глубину
+            var stackTree = new Stack();
+            stackTree.Push(current);
+            while (stackTree.Count != 0)
+            {
+
+                TreeNode_Coll tmp = (TreeNode_Coll)stackTree.Pop();
+                Console.WriteLine(tmp.Value);
+
+                if (tmp.RightChild != null)
+                {
+                    stackTree.Push(tmp.RightChild);
+                }
+                if (tmp.LeftChild != null)
+                {
+                    stackTree.Push(tmp.LeftChild);
+                }
+
+            }
+        }
+    }
+}
---
+++ Exercises/Lesson5_Exercise1_Algs/TreeLogic_Coll.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5_Exercise1_Algs
+{
+
+    public class TreeLogic_Coll
+    {
+
+        TreeNode_Coll Root { get; set; } = null;
+
+
+        public void AddItem(int value)
+        {
+
+            if(Root == null)
+            {
+                Root = new TreeNode_Coll { Value = value };
+                return;
+            }
+
+            var currentNode = Root;
+
+            while (currentNode != null)
+            {
+                if (value > currentNode.Value)
+                {
+                    if (currentNode.RightChild != null)
+                    {
+                        currentNode = currentNode.RightChild;
+                        continue;
+
+                    }
+                    else
+                    {
+                        currentNode.RightChild = new TreeNode_Coll { Value = value, Parent = currentNode };
+                        return;
+                    }
+                }
+                else
+                {
+
+                    if (currentNode.LeftChild != null)
+                    {
+                        currentNode = currentNode.LeftChild;
+                        continue;
+
+                    }
+                    else
+                    {
+                        currentNode.LeftChild = new TreeNode_Coll { Value = value, Parent = currentNode };
+                        return;
+                    }
+                }
+
+
+            }
+
+            throw new NullReferenceException();
+        }
+
+
+
+        public TreeNode_Coll GetNodeByValue(int value)
+        {
+            if(Root == null)
+            {
+                return null;
+            }
+
+            var currentNode = Root;
+
+            while(currentNode != null)
+            {
+                if(currentNode.Value == value)
+                {
+                    return currentNode;
+                }
+
+                if (value > currentNode.Value)
+                {
+                    if (currentNode.RightChild != null)
+                    {
+                        currentNode = currentNode.RightChild;
+                        continue;
+
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                else
+                {
+
+                    if (currentNode.LeftChild != null)
+                    {
+                        currentNode = currentNode.LeftChild;
+                        continue;
+
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+
+            }
+
+            return null;
+
+        }
+
+        public TreeNode_Coll GetRoot()
+        {
+            return Root;
+        }
+
+        public void PrintTree(TreeNode_Coll root, string space = "")
+        {
+            if(root != null)
+            {
+                PrintTree(root.RightChild, space + "\t");
+                Console.WriteLine($"{space}{root.Value}");
+                PrintTree(root.LeftChild, space + "\t");
+
+            }
+        }
+
+        public void RemoveItem(int value)
+        {
+            var currentNode = GetNodeByValue(value);
+
+            if(currentNode != null)
+            {
+                if (currentNode.Value == value)
+                {
+                    //Корень
+                    if (currentNode.Parent == null && currentNode.LeftChild == null && currentNode.RightChild == null)
+                    {
+                        Root = null;
+                        return;
+                    }
+                    if (currentNode.Parent == null && currentNode.RightChild != null && currentNode.LeftChild == null)
+                    {
+                        Root = currentNode.RightChild;
+                        Root.Parent = null;
+                        currentNode.RightChild = null;                       
+                        return;
+                    }
+                    if (currentNode.Parent == null && currentNode.RightChild == null && currentNode.LeftChild != null)
+                    {
+                        Root = currentNode.LeftChild;
+                        Root.Parent = null;
+                        currentNode.LeftChild = null;
+                        return;
+                    }
+                    //Листья
+                    //Если у узла нет наследников, то удаляем ссылку на него, у его родителя.
+                    if (currentNode.Parent != null && currentNode.LeftChild == null && currentNode.RightChild == null)
+                    {
+                        if (currentNode.Equals(currentNode.Parent.LeftChild))
+                        {
+                            currentNode.Parent.LeftChild = null;
+                            return;
+                        }
+                        else if (currentNode.Equals(currentNode.Parent.RightChild))
+                        {
+                            currentNode.Parent.RightChild = null;
+                            return;
+                        }
+
+                        throw new Exception("Something goes wrong! Imposible to delete current item.");
+                    }
+
+                    //Если у узла есть только левый наследник, то он становится на место удаляемого узла.
+                    if (currentNode.Parent != null && currentNode.LeftChild != null && currentNode.RightChild == null)
+                    {
+                        if (currentNode.Equals(currentNode.Parent.LeftChild))
+                        {
+                            currentNode.LeftChild.Parent = currentNode.Parent;
+                            currentNode.Parent.LeftChild = currentNode.LeftChild;
+                            currentNode.Parent = null;
+                            currentNode.LeftChild = null;
+                            return;
+                        }
+                        if (currentNode.Equals(currentNode.Parent.RightChild))
+                        {
+                            currentNode.LeftChild.Parent = currentNode.Parent;
+                            currentNode.Parent.RightChild = currentNode.LeftChild;
+                            currentNode.Parent = null;
+                            currentNode.LeftChild = null;
+                            return;
+                        }
+
+                        throw new Exception("Something goes wrong! Imposible to delete current item.");
+                    }
+                    //Если у узла есть только правый наследник, то он становится на место удаляемого узла.
+                    if (currentNode.Parent != null && currentNode.LeftChild == null && currentNode.RightChild != null)
+                    {
+                        if (currentNode.Equals(currentNode.Parent.LeftChild))
+                        {
+                            currentNode.RightChild.Parent = currentNode.Parent;
+                            currentNode.Parent.LeftChild = currentNode.RightChild;
+                            currentNode.Parent = null;
+                            currentNode.RightChild = null;
+                            return;
+                        }
+                        if (currentNode.Equals(currentNode.Parent.RightChild))
+                        {
+                            currentNode.RightChild.Parent = currentNode.Parent;
+                            currentNode.Parent.RightChild = currentNode.RightChild;
+                            currentNode.Parent = null;
+                            currentNode.RightChild = null;
+                            return;
+                        }
+
+                        throw new Exception("Something goes wrong! Imposible to delete current item.");
+                    }
+                    //Если у узла есть оба наследника.
+                    if (currentNode.LeftChild != null && currentNode.RightChild != null)
+                    {
+
+                        //Если это корень
+                        if(currentNode.Parent == null)
+                        {
+                            Root = currentNode;
+                            bool changing = true;
+                            while (changing)
+                            {
+                                if (currentNode.RightChild != null)
+                                {
+                                    currentNode.Value = currentNode.RightChild.Value;
+                                    currentNode = currentNode.RightChild;
+                                }
+                                else
+                                {
+                                    if(currentNode.LeftChild != null)
+                                    {
+                                        currentNode.Parent.RightChild = currentNode.LeftChild;
+                                        currentNode.LeftChild.Parent = currentNode.Parent;
+                                        currentNode.Parent.RightChild = null;
+                                        currentNode.Parent = null;
+                                        currentNode.LeftChild = null;
+                                        changing = false;
+
+                                    }
+                                    else
+                                    {
+                                        currentNode.Parent.RightChild = null;
+                                        currentNode.Parent = null;
+                                        changing = false;
+                                    }
+                                }
+                            }
+
+                        }
+                        else //Если это узлы
+                        {
+                            bool changing = true;
+                            if (currentNode.Value > currentNode.Parent.Value)
+                            {
+                                
+                                while (changing)
+                                {
+                                    if (currentNode.RightChild != null)
+                                    {
+                                        currentNode.Value = currentNode.RightChild.Value;
+                                        currentNode = currentNode.RightChild;
+
+
+                                    }
+                                    else
+                                    {
+                                        if (currentNode.LeftChild != null)
+                                        {
+                                            currentNode.Parent.RightChild = currentNode.LeftChild;
+                                            currentNode.LeftChild.Parent = currentNode.Parent;
+                                            currentNode.Parent.RightChild = null;
+                                            currentNode.Parent = null;
+                                            currentNode.LeftChild = null;
+                                            changing = false;
+
+                                        }
+                                        else
+                                        {
+                                            currentNode.Parent.RightChild = null;
+                                            currentNode.Parent = null;
+                                            changing = false;
+                                        }
+                                    }
+                                }
+
+                            }
+                            else
+                            {
+                                while (changing)
+                                {
+                                    if (currentNode.LeftChild != null)
+                                    {
+                                        currentNode.Value = currentNode.LeftChild.Value;
+                                        currentNode = currentNode.LeftChild;
+
+
+                                    }
+                                    else
+                                    {
+                                        if (currentNode.RightChild != null)
+                                        {
+                                            currentNode.Parent.LeftChild = currentNode.RightChild;
+                                            currentNode.LeftChild.Parent = currentNode.Parent;
+                                            currentNode.Parent.LeftChild = null;
+                                            currentNode.Parent = null;
+                                            currentNode.RightChild = null;
+                                            changing = false;
+
+                                        }
+                                        else
+                                        {
+                                            currentNode.Parent.LeftChild = null;
+                                            currentNode.Parent = null;
+                                            changing = false;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+
+                }
+
+            }
+        }
+    }
+}
