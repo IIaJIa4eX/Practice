066b59139d694d572ee58441221be2eee220fe98
SinCera_w <iiajia4ex@gmail.com>
2021-11-17 17:05:18 +0300

    Added lesson6

---
+++ Exercises/Lesson6_Exercise1_Algs/GraphEdge.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6_Exercise1_Algs
+{
+    class GraphEdge
+    {
+        public int Weight { get; set; }
+        public GraphNode Node { get; set; }
+
+    }
+}
---
+++ Exercises/Lesson6_Exercise1_Algs/GraphNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6_Exercise1_Algs
+{
+     class GraphNode
+    {
+        public int Value { get; set; }
+        public List<GraphEdge> Edges { get; set; }
+
+        public GraphNode()
+        {
+            Edges = new List<GraphEdge>();
+        }
+
+        public bool isUsed { get; set; } = false;
+
+    }
+}
