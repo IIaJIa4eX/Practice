88d03c48e72b30ead2e07fc41ccf45ee03024965
IIaJIa4eX <iiajia4ex@gmail.com>
2021-12-26 22:58:44 +0300

    Added lesson 2 exrc 2 OOP

---
+++ Exercises/Lesson2_Exercise1_OOP/BankAccount.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_Exercise1_OOP
+{
+    class BankAccount
+    {
+        public enum AccType { Default, VIP, Slave }
+
+        private int _accNumber;
+
+        private static int _numbGenerator;
+
+        private int _balance;
+
+        private AccType _accT;
+
+
+
+        public BankAccount()
+        {
+            _accNumber = _numbGenerator++;
+        }
+
+        public BankAccount(int balance)
+        {
+            _balance = balance;
+            _accNumber = _numbGenerator++;
+        }
+
+        public BankAccount(AccType accType)
+        {
+            _accT = accType;
+            _accNumber = _numbGenerator++;
+        }
+
+        public BankAccount(int balance, AccType accType)
+        {
+            _balance = balance;
+            _accT = accType;
+            _accNumber = _numbGenerator++;// не стал делать метод.
+        }
+
+
+        public int AccNumb
+        {
+            get
+            {
+                return _accNumber;
+            }
+        }
+
+        public int Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                _balance = value;
+            }
+        }
+
+
+        public AccType AccountType
+        {
+            get
+            {
+                return _accT;
+            }
+            set
+            {
+                _accT = value;
+            }
+        }
+
+        public void WithdrawAmount(int amount)
+        {
+            if (_balance - amount < 0)
+                return;
+            _balance -= amount;
+
+        }
+
+        public void Deposit(int amount)
+        {
+            _balance += amount;
+
+        }
+
+        //public int GetAccNumber()
+        //{
+        //    return _accNumber;
+        //}
+
+
+
+
+        //public void SetAccNumber()
+        //{
+        //     _accNumber += 1;
+
+        //}
+
+        //public int GetBalance()
+        //{
+        //    return _balance;
+        //}
+
+
+
+        //public void Setbalance(int balance)
+        //{
+        //    this._balance += balance;
+
+        //}
+
+
+
+
+        //public AccType GetAccType()
+        //{
+
+        //    return _accT;
+        //}
+
+        //public void SetAccType(AccType accType)
+        //{
+        //    if (accType.Equals(AccType.Slave))
+        //        _accT = AccType.Slave;
+
+        //    if(accType.Equals(AccType.VIP))
+        //        _accT = AccType.VIP;
+
+        //}
+    }
+}
---
+++ Exercises/Lesson2_Exercise1_OOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson2_Exercise1_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ss = new BankAccount(45678, BankAccount.AccType.VIP);
+
+            BankAccount ss1 = new BankAccount();
+
+            BankAccount ss2 = new BankAccount();
+            ss2.AccountType = BankAccount.AccType.Slave;
+            ss2.Balance = 78102;
+            ss2.WithdrawAmount(78101);
+            ss1.Deposit(55555);
+
+            Console.WriteLine($"Баланс:{ss.Balance}  Тип:{ss.AccountType} Номер:{ss.AccNumb}");
+            Console.WriteLine($"Баланс:{ss1.Balance}  Тип:{ss1.AccountType} Номер:{ss1.AccNumb}");
+            Console.WriteLine($"Баланс:{ss2.Balance}  Тип:{ss2.AccountType} Номер:{ss2.AccNumb}");
+        }
+    }
+}
