120850a03aa816644d31b8d7141d101709d093c1
SinCera_w <iiajia4ex@gmail.com>
2022-05-03 16:02:47 +0300

    added WPF project

---
+++ Exercises/MetricsDeskTopClient/UserControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MetricsDeskTopClient
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl.xaml
+    /// </summary>
+    public partial class UserControl : UserControl, INotifyPropertyChanged
+    {
+        public UserControl()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
